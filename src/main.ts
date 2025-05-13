73a952874acf30fa0fd09a47d9ddd02e5d6f683d
NDKhanh1996 <ndkhanh1996@gmail.com>
2025-05-13 13:27:13 +0700

    chore: run lint and delete spare code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import type { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import type { ExpressAdapter } from '@nestjs/platform-express';
+
+import { AppModule } from '~/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app: INestApplication<ExpressAdapter> = await NestFactory.create(AppModule);
+
+    await app.listen(process.env.PORT ?? 8080);
 }
-bootstrap();
+void bootstrap();
