e59ee85ffa9e651fd2b1a8ed0bd14941e5538f91
NDKhanh1996 <ndkhanh1996@gmail.com>
2025-05-13 11:52:06 +0700

    setup: config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,65 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error', 'clear'] }],
+      'no-debugger': 'error',
+      'no-dupe-keys': 'error',
+      'no-duplicate-case': 'error',
+      'no-empty': 'error',
+      'no-extra-semi': 'error',
+      'no-func-assign': 'error',
+      'no-obj-calls': 'error',
+      'no-sparse-arrays': 'error',
+      'no-unreachable': 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'valid-typeof': 'warn',
+      curly: 'error',
+      eqeqeq: 'warn',
+      'no-else-return': 'warn',
+      'no-eval': 'error',
+      'semi-spacing': 'error',
+      'keyword-spacing': 'error',
+      'space-infix-ops': 'error',
+      'prefer-const': 'error',
+      'no-restricted-imports': [
+          'error',
+          {
+              patterns: ['.*'],
+          },
+      ],
+      'object-curly-spacing': ['error', 'always'],
+      'padding-line-between-statements': [
+          'error',
+          {
+              blankLine: 'always',
+              prev: '*',
+              next: 'return',
+          },
+          {
+              blankLine: 'always',
+              prev: ['const', 'let', 'var'],
+              next: '*',
+          },
+          {
+              blankLine: 'any',
+              prev: ['const', 'let', 'var'],
+              next: ['const', 'let', 'var'],
+          },
+          {
+              blankLine: 'always',
+              prev: 'directive',
+              next: '*',
+          },
+          {
+              blankLine: 'any',
+              prev: 'directive',
+              next: 'directive',
+          },
+      ],
     },
   },
 );
