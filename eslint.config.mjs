6c33eb7f1fc62a934acbb821da1ba9c4b7145ab9
NDKhanh96 <ndkhanh96@gmail.com>
2025-10-27 14:46:38 +0700

    chore: linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,96 +1,95 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    {
+        ignores: ['eslint.config.mjs'],
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      'prettier/prettier': 'error',
-      'no-console': ['warn', { allow: ['warn', 'error', 'clear'] }],
-      'no-debugger': 'error',
-      'no-dupe-keys': 'error',
-      'no-duplicate-case': 'error',
-      'no-empty': 'error',
-      'no-extra-semi': 'error',
-      'no-func-assign': 'error',
-      'no-obj-calls': 'error',
-      'no-sparse-arrays': 'error',
-      'no-unreachable': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
-      ],
-      'valid-typeof': 'warn',
-      curly: 'error',
-      eqeqeq: 'warn',
-      'no-else-return': 'warn',
-      'no-eval': 'error',
-      'semi-spacing': 'error',
-      'keyword-spacing': 'error',
-      'space-infix-ops': 'error',
-      'prefer-const': 'error',
-      'no-restricted-imports': [
-          'error',
-          {
-              patterns: ['.*'],
-          },
-      ],
-      'object-curly-spacing': ['error', 'always'],
-      'padding-line-between-statements': [
-          'error',
-          {
-              blankLine: 'always',
-              prev: '*',
-              next: 'return',
-          },
-          {
-              blankLine: 'always',
-              prev: ['const', 'let', 'var'],
-              next: '*',
-          },
-          {
-              blankLine: 'any',
-              prev: ['const', 'let', 'var'],
-              next: ['const', 'let', 'var'],
-          },
-          {
-              blankLine: 'always',
-              prev: 'directive',
-              next: '*',
-          },
-          {
-              blankLine: 'any',
-              prev: 'directive',
-              next: 'directive',
-          },
-      ],
     },
-  },
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+            'prettier/prettier': 'error',
+            'no-console': ['warn', { allow: ['warn', 'error', 'clear'] }],
+            'no-debugger': 'error',
+            'no-dupe-keys': 'error',
+            'no-duplicate-case': 'error',
+            'no-empty': 'error',
+            'no-extra-semi': 'error',
+            'no-func-assign': 'error',
+            'no-obj-calls': 'error',
+            'no-sparse-arrays': 'error',
+            'no-unreachable': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            'valid-typeof': 'warn',
+            curly: 'error',
+            eqeqeq: 'warn',
+            'no-else-return': 'warn',
+            'no-eval': 'error',
+            'semi-spacing': 'error',
+            'keyword-spacing': 'error',
+            'space-infix-ops': 'error',
+            'prefer-const': 'error',
+            'no-restricted-imports': [
+                'error',
+                {
+                    patterns: ['.*'],
+                },
+            ],
+            'object-curly-spacing': ['error', 'always'],
+            'padding-line-between-statements': [
+                'error',
+                {
+                    blankLine: 'always',
+                    prev: '*',
+                    next: 'return',
+                },
+                {
+                    blankLine: 'always',
+                    prev: ['const', 'let', 'var'],
+                    next: '*',
+                },
+                {
+                    blankLine: 'any',
+                    prev: ['const', 'let', 'var'],
+                    next: ['const', 'let', 'var'],
+                },
+                {
+                    blankLine: 'always',
+                    prev: 'directive',
+                    next: '*',
+                },
+                {
+                    blankLine: 'any',
+                    prev: 'directive',
+                    next: 'directive',
+                },
+            ],
+        },
+    },
 );
